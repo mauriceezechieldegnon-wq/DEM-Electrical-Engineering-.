be20494817bcb472cfa2e17d813b60a2dfd3708e
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2025-10-07 14:39:09 +0100

    Add files via upload

--- script-2.js
+++ script-2.js
@@ -7,7 +7,11 @@ const tel = document.getElementById("tel").value;
 const service = document.getElementById("service").value;  
 const message = document.getElementById("message").value;  
 const whatsappNumber = "22958660666";  
-const texte = `Bonjour, je m'appelle :{nom}. Je souhaite un devis pour : service.Détails :{message}. Email : email WhatsApp :{tel}`;  
+const texte = `Bonjour, je m'appelle :nom. 
+Je souhaite un devis pour : service.
+Détails :message. 
+Email : email 
+WhatsApp :tel`;  
 const lien = `https://wa.me/whatsappNumber?text={encodeURIComponent(texte)}`;  window.open(lien, "_blank");
 });
 
